3e6053b62fbd72c0faa937e89c62b202c687bc28
Pranay Harjai <pranayharjai7@gmail.com>
2022-12-26 16:22:36 +0100

    Initial Commit

---
+++ app/src/main/java/com/pranayharjai7/myemotions/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pranayharjai7.myemotions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
