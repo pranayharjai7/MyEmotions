a588acf23e92e13f0c0344bf717a48f14b2c8861
Pranay Harjai <pranayharjai7@gmail.com>
2022-12-27 16:25:41 +0100

    Navigation Menu added. Permissions Added. Camera Activity Added.

---
+++ app/src/main/java/com/pranayharjai7/myemotions/LauncherActivity.java
@@ -0,0 +1,35 @@
+package com.pranayharjai7.myemotions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.pranayharjai7.myemotions.databinding.ActivityLauncherBinding;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private ActivityLauncherBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLauncherBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        Runnable runnable = () -> {
+            startActivity(intent);
+            finish();
+        };
+
+        binding.LauncherImage.postDelayed(runnable, 2000);
+    }
+
+    public void launcherConstraintLayoutClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/pranayharjai7/myemotions/MainActivity.java
+++ app/src/main/java/com/pranayharjai7/myemotions/MainActivity.java
@@ -1,16 +1,105 @@
 package com.pranayharjai7.myemotions;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.pranayharjai7.myemotions.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ALL_PERMISSIONS_CODE = 101;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.mainBottomNavigationView.setBackground(null);
 
+        permissions();
+    }
+
+    public void plusFloatingActionButtonClicked(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher cameraLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                binding.thumbnailImageView.setImageBitmap((Bitmap) result.getData().getExtras().get("data"));
+            });
+
+
+
+    public void homeMenuItemClicked(MenuItem item) {
+        if (!item.isChecked()) {
+            item.setChecked(true);
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void statsMenuItemClicked(MenuItem item) {
+        if (!item.isChecked()) {
+            item.setChecked(true);
+        }
+    }
+
+    public void mapsMenuItemClicked(MenuItem item) {
+        if (!item.isChecked()) {
+            item.setChecked(true);
+        }
+    }
+
+    public void moreMenuItemClicked(MenuItem item) {
+        if (!item.isChecked()) {
+            item.setChecked(true);
+        }
+    }
+
+
+
+    private void permissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            requestPermissions(new String[]{
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.INTERNET
+            }, ALL_PERMISSIONS_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == ALL_PERMISSIONS_CODE) {
+            if (grantResults.length != 0) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED
+                        || grantResults[2] != PackageManager.PERMISSION_GRANTED || grantResults[3] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Oops!! You didn't allow permissions!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
