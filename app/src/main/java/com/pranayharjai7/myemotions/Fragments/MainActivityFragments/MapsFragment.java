1bd226f5c4f09e9cc0b61f680333ee71002f1a33
Pranay Harjai <pranayharjai7@gmail.com>
2023-03-17 03:51:02 +0100

    Feature added to get Device location and utilize it.

--- app/src/main/java/com/pranayharjai7/myemotions/Fragments/MainActivityFragments/MapsFragment.java
+++ app/src/main/java/com/pranayharjai7/myemotions/Fragments/MainActivityFragments/MapsFragment.java
@@ -1,19 +1,26 @@
 package com.pranayharjai7.myemotions.Fragments.MainActivityFragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.pranayharjai7.myemotions.R;
 import com.pranayharjai7.myemotions.databinding.FragmentMapsBinding;
@@ -22,6 +29,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentMapsBinding binding;
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public MapsFragment() {
         super(R.layout.fragment_maps);
@@ -38,17 +46,33 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 .findFragmentById(R.id.google_map);
         supportMapFragment.getMapAsync(this);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng myHouse = new LatLng(47.5, 21.6);
         MarkerOptions marker = new MarkerOptions();
-        marker.position(sydney);
-        marker.title("Sydney Marker");
+        marker.position(myHouse);
+        marker.title("Home");
+        marker.snippet("Emotion: Happy");
         mMap.addMarker(marker);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myHouse));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myHouse, 16f));
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
+            if (location != null) {
+                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                //Do something with current location
+            } else {
+                Toast.makeText(getContext(), "Could not get current location. Please try again later.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
