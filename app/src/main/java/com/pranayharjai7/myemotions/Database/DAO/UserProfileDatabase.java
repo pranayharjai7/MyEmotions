f6149e21cca217dc8fb1a2a9ba9dad0930480b01
Pranay Harjai <pranayharjai7@gmail.com>
2023-03-18 00:08:39 +0100

    Removing Unnecessary converters

--- app/src/main/java/com/pranayharjai7/myemotions/Database/DAO/UserProfileDatabase.java
+++ app/src/main/java/com/pranayharjai7/myemotions/Database/DAO/UserProfileDatabase.java
@@ -2,14 +2,10 @@ package com.pranayharjai7.myemotions.Database.DAO;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 
 import com.pranayharjai7.myemotions.Database.UserProfile;
-import com.pranayharjai7.myemotions.Utils.EmotionJSONConverter;
-import com.pranayharjai7.myemotions.Utils.FriendsJSONConverter;
 
 @Database(entities = {UserProfile.class}, version = 1, exportSchema = false)
-@TypeConverters({EmotionJSONConverter.class, FriendsJSONConverter.class})
 public abstract class UserProfileDatabase extends RoomDatabase {
     public abstract UserProfileDAO userProfileDAO();
 }
