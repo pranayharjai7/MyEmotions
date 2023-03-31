2f7f37e81d88996327aa0f74bccdc54b8d274610
Pranay Harjai <67492546+pranayharjai7@users.noreply.github.com>
2023-03-31 21:53:11 +0200

    addFriendsOnMap feature implemented on MapsFragment. Friends mood and location are shown if their mood visibility is public or friends.

--- app/src/main/java/com/pranayharjai7/myemotions/Fragments/MainActivityFragments/MapsFragment.java
+++ app/src/main/java/com/pranayharjai7/myemotions/Fragments/MainActivityFragments/MapsFragment.java
@@ -20,10 +20,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pranayharjai7.myemotions.Database.Friend;
+import com.pranayharjai7.myemotions.Database.UserProfile;
 import com.pranayharjai7.myemotions.R;
+import com.pranayharjai7.myemotions.Utils.Enums.MoodVisibility;
+import com.pranayharjai7.myemotions.Utils.Interfaces.Callback;
 import com.pranayharjai7.myemotions.Utils.LatLngUtils;
 import com.pranayharjai7.myemotions.databinding.FragmentMapsBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
 
@@ -58,12 +68,9 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng myHouse = new LatLng(47.5399011, 21.6226994);
-        MarkerOptions marker = new MarkerOptions();
-        marker.position(myHouse);
-        marker.title("Home");
-        marker.snippet("Emotion: Happy");
-        mMap.addMarker(marker);
+
+        addFriendsOnMap();
+
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(myHouse));
         //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myHouse, 16f));
         mMap.setMyLocationEnabled(true);
@@ -71,6 +78,91 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
         mMap.setOnMyLocationButtonClickListener(this);
 
         getLastLocationOfDevice();
+    }
+
+    private void addFriendsOnMap() {
+        getFriendsFromFirebase(friends -> {
+            getFriendsUserProfiles(friends, userProfiles -> {
+                for (UserProfile userProfile: userProfiles) {
+                    if (userProfile.getLocation().equals("")) {
+                        continue;
+                    }
+
+                    Double lat = Double.parseDouble(userProfile.getLocation().split(",")[0]);
+                    Double lng = Double.parseDouble(userProfile.getLocation().split(",")[1]);
+                    LatLng userLocation = new LatLng(lat,lng);
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(userLocation);
+                    markerOptions.title(userProfile.getUsername());
+                    markerOptions.snippet(userProfile.getLatestEmotion());
+                    mMap.addMarker(markerOptions).showInfoWindow();
+                }
+
+            });
+        });
+
+    }
+
+    private void getFriendsFromFirebase(Callback<List<Friend>> callback) {
+        firebaseDatabase.getReference("MyEmotions")
+                .child("UserProfile")
+                .child(mAuth.getCurrentUser().getUid())
+                .child("friends")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        List<Friend> friends = new ArrayList<>();
+                        if (snapshot.exists()) {
+                            for (DataSnapshot friendSnapshot : snapshot.getChildren()) {
+                                String friendId = friendSnapshot.getKey();
+                                Friend friend = new Friend(mAuth.getCurrentUser().getUid(), friendId);
+                                friends.add(friend);
+                            }
+                            callback.onSuccess(friends);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void getFriendsUserProfiles(List<Friend> friends, Callback<List<UserProfile>> callback) {
+        firebaseDatabase.getReference("MyEmotions")
+                .child("UserProfile")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        List<UserProfile> userProfiles = new ArrayList<>();
+                        for (DataSnapshot friendSnapshot : snapshot.getChildren()) {
+                            for (Friend friend : friends) {
+                                if (friend.getFriendId().equals(friendSnapshot.getKey())) {
+                                    String moodVisibility = friendSnapshot.child("moodVisibility").getValue(String.class);
+                                    if (!moodVisibility.equals(MoodVisibility.ONLYME.toString())) {
+                                        String userId = friendSnapshot.getKey();
+                                        String username = friendSnapshot.child("username").getValue(String.class);
+                                        String email = friendSnapshot.child("email").getValue(String.class);
+                                        String latestEmotion = friendSnapshot.child("latestEmotion").getValue(String.class);
+                                        String latestEmotionDateTime = friendSnapshot.child("latestEmotionDateTime").getValue(String.class);
+                                        String location = friendSnapshot.child("location").getValue(String.class);
+                                        UserProfile userProfile = new UserProfile(userId, username, email, location, moodVisibility, latestEmotion, latestEmotionDateTime);
+                                        userProfiles.add(userProfile);
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        callback.onSuccess(userProfiles);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
     }
 
     @Override
@@ -84,6 +176,8 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
             if (location != null) {
                 LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f));
                 setLastLocationOfUserInFirebase(currentLocation);
             } else {
                 Toast.makeText(getContext(), "Could not get current location. Please try again later.", Toast.LENGTH_LONG).show();
